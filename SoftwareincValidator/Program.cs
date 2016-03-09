59c82248107c21b53e0794cec77a425c101a14fb
Justin Davis <jdphenix@gmail.com>
2016-03-08 23:15:53 -0600

    Initial commit, rough idea of serializing scenarios

---
+++ SoftwareincValidator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SoftwareincValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scenario scen = new Scenario
+            {
+                Name = "Test",
+                Money = new uint[] { 5000, 15000, 35000 },
+                Goals = new[] { "Money 200000" },
+                Years = new ushort[] { 1976, 1978 },
+                Simulation = ScenarioSimulation.TRUE,
+                SimulationSpecified = true, 
+                Trees = ScenarioTrees.FALSE,
+                TreesSpecified = true, 
+                ForceEnvironment = 3,
+                ForceEnvironmentSpecified = true, 
+                Events = new string[0]
+            };
+
+            XmlSerializer ser = new XmlSerializer(typeof(Scenario));
+            using (var writer = new StreamWriter(@"scenario.xml"))
+            {
+                ser.Serialize(writer, scen);
+            }
+        }
+    }
+}
