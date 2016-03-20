e8f4a9e6fc213814afa8c58f489a0948432b53cb
Justin Davis <jdphenix@gmail.com>
2016-03-20 14:02:55 -0500

    Shuffle around generated DTOs to match namespaces to path

---
+++ SoftwareincValidator/Model/Generated/SoftwareType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftwareincValidator.Model.Generated
+{
+    // This is not really a generated class, but it's purpose (a DTO) is the
+    // same as the other generated code in this project. 
+    public class SoftwareType
+    {
+    }
+}
