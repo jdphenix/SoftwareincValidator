21f811901626142218ba05338836cce8eb640db9
Justin Davis <jdphenix@gmail.com>
2016-03-23 22:16:05 -0500

    Implemented validation that researched features have an unlock date.

---
+++ SoftwareincValidator/Validation/Mod/ResearchedFeaturesMustHaveUnlockDate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SoftwareincValidator.Model.Generated;
+
+namespace SoftwareincValidator.Validation.Mod
+{
+    class ResearchedFeaturesMustHaveUnlockDate : IModComponentValidator<SoftwareType>
+    {
+        public IEnumerable<ValidationResult> Validate(SoftwareType component)
+        {
+            return component.Features
+                .Where(x => !string.IsNullOrEmpty(x.Research))
+                .Where(x => x.Unlock < 1970)
+                .Select(x => new ValidationResult(
+                    $"Software type {component.Name} defines a researched feature {x.Name} with an invalid unlock date.",
+                    ValidationLevel.Warning));
+        }
+    }
+}
